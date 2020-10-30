b5f6c7ec8ba19aca647c79424316324285d53773
LAB203\LAB203 <thanabordee@it.kmitl.ac.th>
2020-10-30 10:57:30 +0700

    Restructure folder

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title>Hello World</title>
+</head>
+<body>
+        <Font face="EucrosiaUPC" size="7">
+        <center><b>แนะนำตัว</b></b></center></Font>
+        <Font face="EucrosiaUPC">
+        <hr width="80%" color=#ff7256 size="7"></hr>
+        <center><h2>ชื่อ : นางสาวกัลยรักษ์ ชุบผา</h2>
+        <h2>ชื่อเล่น : หมิว</h2></center>
+        <br />
+        <Font face="EucrosiaUPC" size="7">
+        <center><b>เล่าเรื่องตลก</b></b></center></Font>
+        <center><h2> เกิดวันที่ 25 กุมภาพันธ์ ปี 2002 ค่ะ</h2>
+        <h2>รูปที่เห็นคือน้องม้าคลูกชายหนูเองค่ะ</h2>
+        <h2>ถึงน้องจะเกิดปี 1997 แต่ถ้าเรามองเป็นลูกก็เป็นลูก</h2>
+        <img src="1.jpg" width="250" height="300">
+
+        <img src="2.jpg" width="250" height="300">
+        </center>
+        <br />
+        <Font face="EucrosiaUPC" size="7">
+        <a href="https://www.youtube.com/watch?v=gdZLi9oWNZg">สนับสนุนเพลงของพ่อน้องม้าคนะคะ</a>
+        </Font>
+        </Font>
+    </body>
+</html>
