fa57783915b242bd4bb207f1c05ba5e1a9308277
Zalmwxk <73679596+kalyarak@users.noreply.github.com>
2020-11-17 21:28:45 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,9 +1,16 @@
 <html>
 <head>
-<title>ITF Lab</title>
+  <title>ITF Lab</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
 <?php
+
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'itflab.mysql.database.azure.com', 'miew@itflab', 'Mw7456891023', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
@@ -12,20 +19,24 @@ if (mysqli_connect_errno($conn))
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-<table width="600" border="1">
-  <tr>
-    <th width="100"> <div align="center">name</div></th>
-    <th width="350"> <div align="center">comment </div></th>
-    <th width="150"> <div align="center">link </div></th>
-  </tr>
+<div class="container">
+<table class="table">
+    <thead class="thead-dark">
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+
 <?php
-while($Result = mysqli_fetch_array($res))
+while($result = mysqli_fetch_array($res))
 {
 ?>
   <tr>
-    <td><?php echo $Result['name'];?></div></td>
-    <td><?php echo $Result['comment'];?></td>
-    <td><?php echo $Result['link'];?></td>
+    <td><?php echo $result['name'];?></div></td>
+    <td><?php echo $result['comment'];?></td>
+    <td><?php echo $result['link'];?></td>
   </tr>
 <?php
 }
