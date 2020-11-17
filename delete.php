a4d862893d854e2a03ef4c56738da9e876c3b950
Zalmwxk <73679596+kalyarak@users.noreply.github.com>
2020-11-17 23:37:44 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -6,7 +6,14 @@ if (mysqli_connect_errno($conn))
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
+if ($_GET['id']){
+    $id = $_GET['id'];
+} else {
+    echo "comment not found";
+    die();
+}
+
 $sql = "DELETE FROM guestbook WHERE id = {$id}";
-$query = mysqli_query($sql);
+$query = mysqli_query($conn, $sql);
 
 echo "<a href=\"show.php"\">BACK</a>";
