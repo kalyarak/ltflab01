d8ce85b07f72b1ab004dfac89d9ca6744c048079
Zalmwxk <73679596+kalyarak@users.noreply.github.com>
2020-11-17 23:03:03 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,4 +1,11 @@
 <?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'itflab.mysql.database.azure.com', 'miew@itflab', 'Mw7456891023', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
 if ($_GET['id']){
     $id = $_GET['id'];
 }else{
