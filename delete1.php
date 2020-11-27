643853e982a37b13f9c5add6b6d598ada4e7946f
Zalmwxk <73679596+kalyarak@users.noreply.github.com>
2020-11-27 10:15:46 +0700

    Add files via upload

---
+++ delete1.php
@@ -0,0 +1,21 @@
+<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'itflab.mysql.database.azure.com', 'miew@itflab', 'It123456789', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+$id = $_GET['id'];
+
+$sql = "DELETE FROM guestbook WHERE id = $id";
+
+
+if (mysqli_query($conn, $sql)) {
+    header('location: show1.php');
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
